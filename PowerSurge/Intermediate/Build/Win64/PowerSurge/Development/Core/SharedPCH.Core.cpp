ba79faefe64558dae87ca33e79f1d2bd727ecfec
Guy <54724504+Willphel@users.noreply.github.com>
2022-04-05 09:11:28 -0400

    MenuFix

---
+++ PowerSurge/Intermediate/Build/Win64/PowerSurge/Development/Core/SharedPCH.Core.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30141
+#include "D:/Users/mcwil/Documents/GitHub/PowerSurge/PowerSurge/Intermediate/Build/Win64/PowerSurge/Development/Core/SharedPCH.Core.h"
---
+++ PowerSurge/Intermediate/Source/PowerSurge.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class PowerSurge : ModuleRules
+{
+	public PowerSurge(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
